c9b40fe89e8e772aad185971e2108b8322d00203
7vanael <kristi.n.norris777@gmail.com>
2025-02-19 18:10:34 -0700

    Good progress on Koans today

--- src/koans/20_java_interop.clj
+++ src/koans/20_java_interop.clj
@@ -3,17 +3,21 @@
 
 (meditations
   "You may have done more with Java than you know"
-  (= __ (class "warfare")) ; hint: try typing (javadoc "warfare") in the REPL
+  (= String (class "warfare")) ; hint: try typing (javadoc "warfare") in the REPL
 
   "The dot signifies easy and direct Java interoperation"
-  (= __ (.toUpperCase "select * from"))
+  (= "SELECT * FROM" (.toUpperCase "select * from"))
 
+ ;;Java methods aren't "first class" functions, so they need to be wrapped in a function
+ ;;This method passes a function and a vector to the map function
   "But instance method calls are very different from normal functions"
-  (= ["SELECT" "FROM" "WHERE"] (map ___ ["select" "from" "where"]))
+  (= ["SELECT" "FROM" "WHERE"] (map #(.toUpperCase %) ["select" "from" "where"]))
 
+ ;;I guessed 10, but couldn't tell you what that java function is except it has to do with
+ ;;threading, which I also don't know how to do yet.
   "Constructing might be harder than breaking"
-  (= 10 (let [latch (java.util.concurrent.CountDownLatch. __)]
+  (= 10 (let [latch (java.util.concurrent.CountDownLatch. 10)]
           (.getCount latch)))
 
   "Static methods are slashing prices!"
-  (== __ (Math/pow 2 10)))
+  (== 1024 (Math/pow 2 10)))
